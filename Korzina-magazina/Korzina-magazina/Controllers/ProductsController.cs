49cc8ed3f2160ebb65cbc576e53723f6d1561860
Dimka122 <kassindima1@gmail.com>
2024-02-05 11:52:11 +0200

    добавление класса инициализации БД и контроллера

---
+++ Korzina-magazina/Korzina-magazina/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Korzina_magazina.Data;
+using Korzina_magazina.Models;
+
+namespace Korzina_magazina.Controllers
+{
+    public class ProductsController:Controller
+    {
+        private readonly ProductContext _context;
+
+        public ProductsController(ProductContext context)
+        {
+            _context = context;
+        }
+
+        
+
+        public async Task<IActionResult> Index()
+        {
+            return _context.Products != null ?
+                        View(await _context.Products.ToListAsync()) :
+                        Problem("Entity set 'ProductContext.Products'  is null.");
+        }
+    }
+}
---
+++ Korzina-magazina/Korzina-magazina/Data/DbInitializer.cs
@@ -0,0 +1,100 @@
+﻿using Korzina_magazina.Models;
+using System;
+using System.Linq;
+
+namespace Korzina_magazina.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(ProductContext context)
+        {
+            context.Database.EnsureCreated();
+            //Look for any products
+            if (context.Products.Any())
+            {
+                return;//DB has been seeded
+            }
+            var products = new Product[]
+            {
+                  new Product()
+                  {
+                      Id = "1",
+                     Name = "Aconitum",
+                      Photo = "Georgina.png",
+                      Price = 2.80
+                  },
+
+                new Product()
+                {
+                    Id="2",
+                    Name = "African Daisy",
+                    Photo = "HZ.png",
+                    Price = 3.80
+                },
+                new Product()
+                {
+                    Id="3",
+                    Name = "Begonia",
+                    Photo = "jordlia.png",
+                    Price = 3.80
+                },
+                  new Product()
+                  {
+                      Id="4",
+                      Name = "Chrysanthemum",
+                      Photo = "Lutik.png",
+                      Price = 3.60
+                  },
+                new Product()
+                {
+                    Id="5",
+                    Name = "Dahlia",
+                    Photo = "Mayory.png",
+                    Price = 6.80
+                },
+                new Product()
+                {
+                    Id="6",
+                    Name = "Daisy",
+                    Photo = "Peon.png",
+                    Price = 1.68
+                },
+                  new Product()
+                  {
+                      Id="7",
+                      Name = "Echinacea",
+                      Photo = "Romshka.png",
+                      Price = 3.80
+                  },
+                  new Product()
+                  {
+                      Id="8",
+                      Name = "ForgetMeNot",
+                      Photo = "Rosa.png",
+                      Price = 3.45
+                  },
+                new Product()
+                {
+                    Id = "9",
+                    Name = "Jasmine",
+                    Photo = "Rozochka.png",
+                    Price = 3.80
+                },
+                new Product()
+                {
+                    Id="10",
+                    Name="Tulpan",
+                    Photo="Tulpan.png",
+                  Price=4.25
+                }
+
+            };
+            foreach (Product p in products)
+            {
+                context.Products.Add(p);
+            }
+            context.SaveChanges();
+
+        }
+    }
+}
